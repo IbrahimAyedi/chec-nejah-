9da8e346b10d1006f092c86212b4e13e8424c026
IbrahimAyedi <139042487+IbrahimAyedi@users.noreply.github.com>
2023-07-30 11:10:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+export default class App extends Component {
+  state = {
+    count: 0,
+    isShow: true,
+  };
+  handleShow = () => this.setState({ isShow: !this.state.isShow });
+  handlePlus = () => this.setState({ count: this.state.count + 1 });
+  handleMois = () =>
+    this.state.count > 0 && this.setState({ count: this.state.count - 1 });
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+  render() {
+    console.log("render");
+    // setInterval(this.handlePlus, 1000);
+
+    return (
+      <div className="App">
+        <button onClick={this.handleShow}>
+          {this.state.isShow ? "Hide" : "Show"}
+        </button>
+        {this.state.isShow && (
+          <>
+            <h1>{this.state.count}</h1>
+
+            <button onClick={this.handlePlus}>+</button>
+            <button onClick={this.handleMois}>-</button>
+          </>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
